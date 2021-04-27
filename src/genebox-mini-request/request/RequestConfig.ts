a348f6f3630ebbae7c52cd2f0b533006fd2a8868
Songlcy <songxiaoliang@users.noreply.github.com>
2021-04-27 21:12:35 +0800

    request loading add enable params
    request loading add enable params

--- src/genebox-mini-request/request/RequestConfig.ts
+++ src/genebox-mini-request/request/RequestConfig.ts
@@ -65,7 +65,7 @@ const ResponseType = (resType: string) => (target, p, descriptor): void => {
 }
 
 // 显示请求 Loading
-const Loading = () => (target, p, descriptor): void => {
+const Loading = (enableLoging: boolean) => (target, p, descriptor): void => {
   if (descriptor) {
     descriptor.value.showLoading = true;
   }
